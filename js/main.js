ba2f86d53acda416a4283b9b599fdf01efa9c64e
Полина Левченко <gr432_leple@lab-203-7.tomtit-tomsk.ru>
2026-03-02 12:24:35 +0700

    done task 4

--- js/main.js
+++ js/main.js
@@ -2,11 +2,21 @@ let app = new Vue({
    el: '#app',
    data: {
        product: "Socks",
-       image: "assets/vmSocks-green-onWhite.jpg",
-       altText: "A pair of soks",
-       link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+       image: "./assets/vmSocks-green-onWhite.jpg",
+       altText: "A pair of socks",
        inStock: true,
-       inventory: 100,
-       onSale: false
+       details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+       variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+        ],
+        sizes:['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+
    }
 })
